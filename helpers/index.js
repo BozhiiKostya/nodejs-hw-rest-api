4d76e1819a344c0b1626ed8dc2cbb06fca38591c
BozhiiKostya <kostyazap@gmail.com>
2024-01-27 22:40:09 +0200

    Completed HW 6

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require('./http-error');
 const ctrlWrapper = require('./ctrl-wrapper');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
+  sendEmail,
 };
